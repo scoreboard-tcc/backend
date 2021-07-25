f7faa6525f78e1379a65229ccedee54e09c7faf1
Jorge Rossi <jorgefzrossi@gmail.com>
2021-07-25 19:40:50 -0300

    Cria o bucket do MinIO se não existir

--- src/useCases/utils/UploadFile/UploadFileUseCase.js
+++ src/useCases/utils/UploadFile/UploadFileUseCase.js
@@ -2,6 +2,29 @@ const { v4: uuid } = require('uuid');
 const minio = require('../../../providers/storage/minio');
 const config = require('../../../config/minio')
 
+async function createBucketIfNotExists() {
+  const exists = (await minio.listBuckets()).find(b => b.name === 'logos')
+
+  if (!exists) {
+    await minio.makeBucket('logos')
+    await minio.setBucketPolicy('logos', JSON.stringify({
+      "Id": "PolicyID",
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "Stmt1610823452352355758",
+          "Action": [
+            "s3:GetObject"
+          ],
+          "Effect": "Allow",
+          "Resource": "arn:aws:s3:::logos/*",
+          "Principal": "*"
+        }
+      ]
+    }))
+  }
+}
+
 class UploadFileUseCase {
   async execute(file) {
     const extension = file.originalname.split('.').pop()
@@ -12,5 +35,7 @@ class UploadFileUseCase {
     return `${config.domain}/logos/${fileName}`;
   }
 }
+
+createBucketIfNotExists()
 
 module.exports = UploadFileUseCase;
